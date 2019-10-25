e27314e4870f2f440b523b6ff38401b7ef022742
danulanang <danu.lanang24@gmail.com>
2019-10-25 13:14:45 +0700

    Add files via upload

---
+++ Temperature_sensor_IOT.ino
@@ -0,0 +1,69 @@
+/*************************************************************
+  Download latest Blynk library here:
+    https://github.com/blynkkk/blynk-library/releases/latest
+
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Follow us:                  http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+
+ *************************************************************
+  This example runs directly on ESP32 chip.
+
+  Note: This requires ESP32 support package:
+    https://github.com/espressif/arduino-esp32
+
+  Please be sure to select the right ESP32 module
+  in the Tools -> Board menu!
+
+  Change WiFi ssid, pass, and Blynk auth token to run :)
+  Feel free to apply it to any other example. It's simple!
+ *************************************************************/
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <BlynkSimpleEsp32_SSL.h>
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+char auth[] = "5fL1TgeHli1DLtfGoRGzaBFB47swfGdC";
+char ssid[] = "Lantai 2";
+char pass[] = "gakbolehtau";
+
+const int oneWireBus = 5;     
+OneWire oneWire(oneWireBus);
+DallasTemperature sensors(&oneWire);
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+
+  sensors.begin();
+}
+
+void loop()
+{
+  sensors.requestTemperatures(); 
+  float temperatureC = sensors.getTempCByIndex(0);
+  Blynk.virtualWrite(V1,temperatureC);
+  Serial.println(temperatureC);
+  Blynk.run();
+}
+
